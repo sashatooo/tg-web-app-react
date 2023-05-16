42762385be68092ad0a7a9e2345e749c143b11be
sashatooo <sashatosasha77@gmail.com>
2023-05-16 16:56:28 +0300

    tg object and more

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import { useEffect } from 'react'
 import './App.css'
+import { useTelegram } from './hooks/useTelegram'
 
-const tg = window.Telegram.WebApp
+
 
 function App() {
+
+	const { onToggleButton, tg } = useTelegram()
 
 	useEffect( () => {
 		tg.ready()
 	}, [])
 
-	const onClose = () => {
-		tg.close()
-	} 
-
 	return (
 		<div className="App">
-			work
-			<button onClick={ onClose }>close</button>
+			<button onClick={ onToggleButton }>toggle</button>
 		</div>
 	);
 }
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,22 @@
+import { useTelegram } from "../../hooks/useTelegram"
+import Button from "../Button/Button"
+
+
+
+const Header = (props) => {
+
+    const { user, onClose } = useTelegram()
+
+
+
+    return (
+        <div className={'header'}>
+            <Button onClick={ onClose }>Close</Button>
+            <span className={'username'}>
+                { user?.username }
+            </span>
+        </div>
+    )
+}
+
+export default Header

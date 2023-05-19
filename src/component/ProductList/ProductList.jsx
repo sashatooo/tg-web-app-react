7979737e3782e52e558e83358ad2f11c4f96d1e3
sashatooo <sashatosasha77@gmail.com>
2023-05-19 20:55:12 +0300

    fix tgHook

--- src/component/ProductList/ProductList.jsx
+++ src/component/ProductList/ProductList.jsx
@@ -23,7 +23,7 @@ const getTotalPrice = (items) => {
 const ProductList = () => {
 
     const [addedItems, setAddedItems] = useState([])
-    const {tg} = useTelegram
+    const {tg} = useTelegram()
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id)
